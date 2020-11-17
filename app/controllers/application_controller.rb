104e96892666df0bd77a32702632221b056aa405
EC2 Default User <ec2-user@ip-172-31-64-135.ec2.internal>
2020-11-17 10:27:46 +0000

    ロゴ追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,16 @@
 class ApplicationController < ActionController::Base
-  
+
   # 検索機能 ransack
   before_action :set_search
   def set_search
     @search = Shop.ransack(params[:q]) #ransackメソッド推奨
     @search_shops = @search.result.all
   end
-  
+
   # ログイン後、遷移ページ
-  def after_sign_in_path_for(resource)
-    root_path
+  def after_sign_up_path_for(resource)
+    complete_path
   end
-  
-  
-  
+
+
 end
